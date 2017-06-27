b6ae4d07f6fe0876b6ce4cb90b1c6655b88d0820
Kristiana Nedelcheva <Krisi@ad.auxcode.com>
2017-06-27 14:58:35 +0300

    Initial commit

---
+++ CasandraApp/CasandraApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Cassandra;
+using System;
+
+namespace CasandraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
+            ISession session = cluster.Connect("demo");
+        }
+    }
+}
