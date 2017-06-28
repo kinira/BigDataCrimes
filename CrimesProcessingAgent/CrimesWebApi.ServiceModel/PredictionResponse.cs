82c8ef3cd9d53fc278110feb100d3495b08d1a9b
Kristiana Nedelcheva <florentina2@abv.bg>
2017-06-28 07:39:48 +0300

    edit service

---
+++ CrimesProcessingAgent/CrimesWebApi.ServiceModel/PredictionResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrimesWebApi.ServiceModel
+{
+    public class PredictionResponse
+    {
+        public double Probability { get; set; }
+    }
+}
--- CrimesProcessingAgent/CrimesWebApi/Service/HomeService.cs
+++ CrimesProcessingAgent/CrimesWebApi/Service/HomeService.cs
@@ -1,5 +1,7 @@
-﻿using CrimesWebApi.ServiceModel;
-using ServiceStack;
+﻿using CrimesProcessing.Contracts;
+using CrimesWebApi.ServiceModel;
+using Grpc.Core;
+using Service = ServiceStack.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,22 @@ namespace CrimesWebApi
 {
     public class HomeService : Service
     {
+        private CrimesService.CrimesServiceClient agent1;
+
+        public HomeService()
+        {
+            var channel1 = new Channel($"127.0.0.1:50051", ChannelCredentials.Insecure);
+            agent1 = new CrimesService.CrimesServiceClient(channel1);
+        }
+
         public object Any(HelloRequest request)
         {
             return new HelloResponse { Result = $"Hello {request.Name}"};
+        }
+
+        public async Task<PredictionResponse> Post(PredictionRequest request)
+        {
+            await agent1.GetProbabilityAsync(new CalculatePredictionRequest { Date = request.Date.ToString(), Xcoordinates = request.X_coordinate.ToString(), Ycoordinates = request.Y_coordinate.ToString() });
         }
 
     }
