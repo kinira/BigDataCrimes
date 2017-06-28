eea15ad39d8beac7c54c999a85ad3cfaddfa0f9b
Kristiana Nedelcheva <florentina2@abv.bg>
2017-06-28 05:12:56 +0300

    Add Redis save

---
+++ CrimesProcessingAgent/Crimes.Processing/Redis/IRedisManager.cs
@@ -0,0 +1,12 @@
+﻿using Crimes.Processing.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crimes.Processing.Redis
+{
+    public interface IRedisManager
+    {
+        void InsertIntoRedis(List<DisctrictScore> disctrictscores);
+    }
+}
